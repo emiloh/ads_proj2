453af690048ff3bdf98552549145a2f8b9894045
emiloh <emil@houlborg.dk>
2024-12-08 21:16:35 +0100

    fix: Minor errors

--- profile.py
+++ profile.py
@@ -6,27 +6,26 @@ def run_queries_get_json(out):
     sfs = ["sf1", "sf10", "sf100"]
     queries = ["q01_1", "q02_3", "q03_4"]
     threads = [1, 2, 4, 8]
-
     for sf in sfs:
-        db = f"duckdb/duckdb_benchmark_data/{sf}.duckdb"
+        db = f"./duckdb/duckdb_benchmark_data/{sf}.duckdb"
         conn = duckdb.connect(db)
         
-        config = """PRAGMA enable_profiling = json";
-        PRAGMA custom_profiling_settings = {"OPERATOR_CARDINALITY": "true", "OPERATOR_ROWS_SCANNED": "true", "OPERATOR_TIMING": "true", "OPERATOR_TYPE": "true"};
+        config = """SET enable_profiling = 'json';
+        SET custom_profiling_settings = '{"OPERATOR_CARDINALITY": "true", "OPERATOR_ROWS_SCANNED": "true", "OPERATOR_TIMING": "true", "OPERATOR_TYPE": "true"}';
         """
-
         conn.sql(config)
 
+
         for thread in threads:
-            conf_threads = f"PRAGMA threads = {thread}"
+            conf_threads = f"SET threads = {thread};"
             conn.sql(conf_threads)
             
             for query in queries:
-                out = f"PRAGMA profiling_output = {out}/json_{query}_{sf}_{thread}.json"
-                conn.sql(out)
+                path = f"SET profile_output = './{out}/{query}_{sf}_{thread}.json'"
+                conn.sql(path)
 
-                q = Path(f"duckdb/benchmark/ssb/queries/{query}.sql").read_text()
-                conn.sql(q)
+                q = Path(f"./duckdb/benchmark/ssb/queries/{query}.sql").read_text()
+                conn.sql(q).show()
 
 
 def create_operator_graphs(path):
