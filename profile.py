bce9b6895e849dfbd05e684136ef237eaf1aca97
emiloh <Emil@houlborg.dk>
2024-12-10 13:35:09 +0100

    feat: Use full detailed profiling

--- profile.py
+++ profile.py
@@ -8,11 +8,13 @@ def run_queries_get_json(out):
     threads = [1, 2, 4, 8]
     for sf in sfs:
         db = f"./duckdb/duckdb_benchmark_data/{sf}.duckdb"
+        print(db)
         conn = duckdb.connect(db)
         
         config = """SET enable_profiling = 'json';
-        SET custom_profiling_settings = '{"OPERATOR_CARDINALITY": "true", "OPERATOR_ROWS_SCANNED": "true", "OPERATOR_TIMING": "true", "OPERATOR_TYPE": "true"}';
+        SET profiling_mode = 'detailed';
         """
+        #SET custom_profiling_settings = '{"OPERATOR_CARDINALITY": "true", "OPERATOR_ROWS_SCANNED": "true", "OPERATOR_TIMING": "true", "OPERATOR_TYPE": "true"}';
         conn.sql(config)
 
 
@@ -21,9 +23,9 @@ def run_queries_get_json(out):
             conn.sql(conf_threads)
             
             for query in queries:
-                path = f"SET profile_output = './{out}/{query}_{sf}_{thread}.json'"
+                path = f"SET profile_output = './{out}/detailed_{query}_{sf}_{thread}.json'"
                 conn.sql(path)
-
+                
                 q = Path(f"./duckdb/benchmark/ssb/queries/{query}.sql").read_text()
                 conn.sql(q).show()
 
